bdb9ae2a02cebcc37bf87399443b710f72f56c59
Omkareshwar-17 <garvofficial17@gmail.com>
2026-02-07 09:15:30 +0530

    Day8

---
+++ Day008/q8.c
@@ -0,0 +1,40 @@
+/*
+Problem: Given integers a and b, compute a^b using recursion without using pow() function.
+
+Input:
+- Two space-separated integers a and b
+
+Output:
+- Print a raised to power b
+
+Example:
+Input:
+2 5
+
+Output:
+32
+*/
+
+#include <stdio.h>
+
+long long power(int a, int b) {
+    if (b == 0) {
+        return 1;
+    }
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    if (scanf("%d %d", &a, &b) != 2) {
+        return 0;
+    }
+
+    if (b < 0) {
+        printf("0\n"); 
+    } else {
+        printf("%lld\n", power(a, b));
+    }
+
+    return 0;
+}
