a2234ddf1deb1945e91995d09c524f1639c8591b
Omkareshwar-17 <garvofficial17@gmail.com>
2026-02-20 09:15:30 +0530

    Day21

---
+++ Day021/q21.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, val;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *last = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            last = head;
+        } else {
+            last->next = newNode;
+            last = newNode;
+        }
+    }
+
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
