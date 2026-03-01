bc3c226fe5240d280a46d936c5bd5964abd01748
Omkareshwar-17 <garvofficial17@gmail.com>
2026-03-01 09:15:30 +0530

    Day31

---
+++ Day031/q31.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        return; // Stack Overflow
+    }
+    stack[++top] = value;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("%d\n", stack[top--]);
+    }
+}
+
+void display() {
+    if (top == -1) {
+        return;
+    }
+    for (int i = top; i >= 0; i--) {
+        printf("%d%s", stack[i], (i == 0 ? "" : " "));
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, choice, val;
+    if (scanf("%d", &n) != 1) return 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                scanf("%d", &val);
+                push(val);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+        }
+    }
+
+    return 0;
+}
