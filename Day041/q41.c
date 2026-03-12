0d3dbec34f0efe05a45e9d77f448cddc72be5d3d
Omkareshwar-17 <garvofficial17@gmail.com>
2026-03-12 09:15:30 +0530

    Day42

--- Day041/q41.c
+++ Day041/q41.c
@@ -1,57 +1,65 @@
-import sys
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+struct Node {
+    int data;
+    struct Node* next;
+};
 
-class Queue:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+struct Queue {
+    struct Node *front, *rear;
+};
 
-    def enqueue(self, data):
-        new_node = Node(data)
-        if self.tail is None:
-            self.head = self.tail = new_node
-            return
-        self.tail.next = new_node
-        self.tail = new_node
+struct Queue* createQueue() {
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
 
-    def dequeue(self):
-        if self.head is None:
-            return -1
-        
-        temp = self.head
-        self.head = self.head.next
-        
-        if self.head is None:
-            self.tail = None
-            
-        return temp.data
+void enqueue(struct Queue* q, int k) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = k;
+    temp->next = NULL;
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+    q->rear->next = temp;
+    q->rear = temp;
+}
 
-def solve():
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-        
-    n = int(input_data[0])
-    q = Queue()
-    
-    idx = 1
-    operations_count = 0
-    while operations_count < n and idx < len(input_data):
-        op = input_data[idx]
-        
-        if op.lower() == "enqueue":
-            val = int(input_data[idx + 1])
-            q.enqueue(val)
-            idx += 2
-        elif op.lower() == "dequeue":
-            print(q.dequeue())
-            idx += 1
-            
-        operations_count += 1
+int dequeue(struct Queue* q) {
+    if (q->front == NULL) {
+        return -1;
+    }
+    struct Node* temp = q->front;
+    int data = temp->data;
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    free(temp);
+    return data;
+}
 
-if __name__ == "__main__":
-    solve()
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+
+    struct Queue* q = createQueue();
+    char op[20];
+    int val;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", op);
+        if (strcmp(op, "enqueue") == 0) {
+            scanf("%d", &val);
+            enqueue(q, val);
+        } else if (strcmp(op, "dequeue") == 0) {
+            printf("%d\n", dequeue(q));
+        }
+    }
+
+    return 0;
+}
